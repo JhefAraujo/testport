3ea5a9e1c56c9c420dec362b715e5c9817d48a24
Jhefferson Araujo <jhefferson.araujo@gmail.com>
2024-04-02 11:31:31 -0300

    best transitions

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function handleclick() {
     this.document.getElementById("sec1").style.display = "block";
     this.document.getElementsByTagName("img")[0].classList.add("headerH1");
+    document.getElementById("language").classList.toggle("whiteLanguage");
     setTimeout(() => {
         this.document.body.style.overflowY = "scroll";
-        document.getElementById("language").classList.toggle("whiteLanguage");
     }, 1000);
     this.removeEventListener("click", arguments.callee);
 }
@@ -14,10 +14,10 @@ document.getElementsByTagName("img")[0].addEventListener("click", function () {
     this.classList.remove("headerH1");
     document.body.style.overflowY = "hidden";
     document.getElementById("sec1").classList.add("sec1Hide");
+    document.getElementById("language").classList.toggle("whiteLanguage");
     setTimeout(() => {
         document.getElementById("sec1").style.display = "none";
         document.getElementById("sec1").classList.remove("sec1Hide");
         window.addEventListener("click", handleclick);
-        document.getElementById("language").classList.toggle("whiteLanguage");
     }, 1300);
 });
