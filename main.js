79045b74250da522b8d2f390923b25560dca866e
Jhefferson Araujo <jhefferson.araujo@gmail.com>
2024-04-02 14:43:35 -0300

    interability to the text

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function handleclick() {
     this.document.getElementById("sec1").style.display = "block";
+    for (let i = 0; i < document.getElementsByClassName('container').length; i++) {
+        const element = document.getElementsByClassName('container')[i];
+        element.style.opacity = 0;
+    }
     this.document.getElementsByTagName("img")[0].classList.add("headerH1");
     setTimeout(() => {
         this.document.body.style.overflowY = "scroll";
@@ -12,6 +16,10 @@ window.addEventListener("click", handleclick);
 document.getElementsByTagName("img")[0].addEventListener("click", function () {
     this.classList.remove("headerH1");
     document.body.style.overflowY = "hidden";
+    for (let i = 0; i < document.getElementsByClassName('container').length; i++) {
+        const element = document.getElementsByClassName('container')[i];
+        element.style.opacity = 1;
+    }
     document.getElementById("sec1").classList.add("sec1Hide");
     setTimeout(() => {
         document.getElementById("sec1").style.display = "none";
